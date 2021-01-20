ad904816ca1056fbb740d2f422f2e3a378af946d
Diego A <esteban.aguilar@televisatim.com>
2021-01-19 18:46:18 -0600

    ADD: Se agrega design sistem setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import merge from 'lodash.merge';
+import get from 'lodash.get';
+import * as TUDN from '../lib/themes/tudn';
+import GlobalStyles from '../lib/accessories/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const theme = ['Light,Dark'];
+  const getTheme = (mode, btheme) =>
+  merge({}, btheme,{
+    color:get(btheme.colors.modes,mode,btheme.colors),
+  });
+  const THEME = getTheme (theme[1],TUDN);
+  return (
+  <ThemeProvider theme={THEME}>
+      <Component {...pageProps} />
+      <GlobalStyles/>
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
